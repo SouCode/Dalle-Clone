b7a6661814d5b7d9dc60ead7375429261370eeff
Sou <samelsonjohn@protonmail.com>
2023-07-01 19:12:32 -0400

    FormField progress

--- client/Dalle-Clone/src/components/FormField.jsx
+++ client/Dalle-Clone/src/components/FormField.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-const FormField = () => {
+const FormField = ({ LabelName, type, name, placeholder, 
+  value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
   return (
-    <div>FormField</div>
-  )
-}
+    <div>
+      <div className='flex items-center gap-2 mb-2'>
+        <label
+          htmlFor={name}
+          className='block text-sm font-medium text-gray-900'
+        >
+          {LabelName} {/* Corrected prop name */}
+        </label>
+      </div>
+    </div>
+  );
+};
 
-export default FormField
+export default FormField;
