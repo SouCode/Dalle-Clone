ed13113961baf668d35b559556a6ad684c24c528
Sou <samelsonjohn@protonmail.com>
2023-07-02 08:21:33 -0400

    Create Post Form progress

--- client/Dalle-Clone/src/components/FormField.jsx
+++ client/Dalle-Clone/src/components/FormField.jsx
@@ -11,9 +11,31 @@ const FormField = ({ LabelName, type, name, placeholder,
         >
           {LabelName} {/* Corrected prop name */}
         </label>
+        {isSurpriseMe && (
+          <button
+            type='button'
+            onClick={handleSurpriseMe}
+            className='font-semibold text-xs bg-[#ECECF1] py-1
+            px-2 rounded-[5px] text-black'
+          >
+            Surprise me
+          </button>
+        )}
       </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className='bg-gray-50 border border-gray-300 text-gray-900
+        text-sm rounded-lg focus:ring-[#4649ff] foxus:border-[4649ff] 
+        outline-none block w-full p-3'
+      />
     </div>
   );
-};
+}
 
 export default FormField;
