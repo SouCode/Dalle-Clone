a70f4604b67f0af0ee1636173a5c0a71cd0a85a2
Sou <samelsonjohn@protonmail.com>
2023-07-02 13:37:55 -0400

    loading ~ Generate/Generating

--- client/Dalle-Clone/src/pages/CreatePost.jsx
+++ client/Dalle-Clone/src/pages/CreatePost.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { preview } from '../assets';
 import { getRandomPrompt } from '../utils';
-import { FormField } from '../components';
+import { FormField, Loader } from '../components';
 
 const CreatePost = () => {
   const navigate = useNavigate();
@@ -13,6 +13,10 @@ const CreatePost = () => {
   });
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
+
+  const generateImage = () => {
+
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -63,9 +67,9 @@ const CreatePost = () => {
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
-          
+        
           <div className=' relative bg-gray-50 border border-gray-300 text-gray-900 text-sm
-          rounded-lg focus:ring-blue-500 focus:border-blie-500 w-64 p-3 h-64 flex
+          rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex
           justify-center items-center'>
             {form.photo ? (
               <img
@@ -80,14 +84,26 @@ const CreatePost = () => {
                 className='w-9/12 h9/12 object-contain opacity-40'
               />
             )}
-          </div>
 
-          <button
-            type="submit"
-            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+            {generatingImg && (
+              <div className='absolute inset-0 z-0 flex justify-center items-center 
+              bg-[rgba(0,0,0,0.5)] rounded-lg'>
+                <Loader />
+              </div>
+            )} 
+          </div>
+        </div>
+
+        <div className='mt-5 flex gap-5'>
+          <button 
+            type='button' 
+            onClick={generateImage}
+            className='text-white bg-green-700 font-medium rounded-md text-sm w-full
+            sm:w-auto px-5 py-2.5 text-center'
           >
-            Create Post
+            {generatingImg ? 'Generating...' : 'Generate'}
           </button>
+
         </div>
       </form>
     </section>
