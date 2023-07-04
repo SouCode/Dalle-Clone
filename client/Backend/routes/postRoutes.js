b87853b739edc6aa44e18ee90a96c88baf2286a2
Sou <samelsonjohn@protonmail.com>
2023-07-04 18:58:33 -0400

    Share Post Backend Test

--- client/Backend/routes/postRoutes.js
+++ client/Backend/routes/postRoutes.js
@@ -5,6 +5,38 @@ require('dotenv').config();
 
 const router = express.Router();
 
-// Your route handlers here
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+// Get All Post
+router.route('/').get(async(req, res) => {
+    try{
+        const post = await Post.find({});
+
+        res.status(200).json({ success: true, data:posts })
+    } catch (error) {
+        res.status(500).json({ success: false, message: error })
+    }
+});
+
+//Get a Post
+router.route('/').get(async(req, res) => {
+    try {
+        const { name, prompt, photo } = req.body;
+        const photoUrl = await cloudinary.uploader.upload(photo);
+    
+        const newPost = await Post.create({
+            name,
+            prompt,
+            photo: photoUrl.url,
+        })
+        res.status(200).json({ success: true, data: newPost });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error});
+    }
+});
 
 module.exports = router;
