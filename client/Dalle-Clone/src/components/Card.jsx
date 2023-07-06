c42c693c1332cf0839edebca11793fcc4a065ff8
Sou <samelsonjohn@protonmail.com>
2023-07-05 21:33:05 -0400

    Images shown in the cards

--- client/Dalle-Clone/src/components/Card.jsx
+++ client/Dalle-Clone/src/components/Card.jsx
@@ -1,9 +1,16 @@
+// Dalle-Clone/src/components/Card.jsx
 import React from 'react'
 
-const Card = () => {
+const Card = ({ _id, name, prompt, photo }) => {
   return (
-    <div>Card</div>
+    <div className='card'>
+      <img src={photo} alt={name} className='card-img-top' />
+      <div className='card-body'>
+        <h5 className='card-title'>{prompt}</h5>
+      </div>
+    </div>
   )
 }
+
 
 export default Card
